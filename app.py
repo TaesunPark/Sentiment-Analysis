4caf7e97f76d962b78c75d72d9cac9c80506d255
tovbskvb@daum.net <tovbskvb@daum.net>
2022-05-18 01:36:43 +0900

    [add] 자소서 질문 추천 app.route 추가

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import librosa
 import numpy as np
 import os
 import test
+import question
 
 app = Flask(__name__)
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
@@ -15,7 +16,6 @@ pad2d = lambda a, i: a[:, 0:i] if a.shape[1] > i else np.hstack((a, np.zeros((a.
 
 @app.route('/')
 def hello_world():  # put application's code here
-
     return 'Hello World!'
 
 
@@ -23,6 +23,12 @@ def hello_world():  # put application's code here
 def audio_test():  # put application's code here
     return load_audio("./test/happy.wav")
 
+
+@app.route('/analysis')
+def analyze_self_introduction():
+    text = "리눅스의 기본명령어를 이해 합니다.\nAWS EC2 서버를 호스팅 하여 소프트웨어 설치 및 config파일을 수정 및 서비스를 구동 할 수 있습니다.\n도커를 이용하여 Flask, Spring-boot 를 배포한 경험이 있습니다.\nNginx를 이용하여 포트 포워딩 및 로드 밸런싱, ssl 인증서를 적용한 경험이 있습니다.\n인프라를 구축하고 무중단 배포한 경험이 있습니다."
+    result = question.get_keyword_summarizer(text)
+    return str(question.chat(result))
 
 if __name__ == '__main__':
     app.run()
